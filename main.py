9b545111c5b92ced1f8be9878626c9da9e7b83b6
dim_g <fewmoments@yahoo.com>
2023-03-24 18:49:37 +0300

    readme changed, files iter added

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from pathlib import Path
 from docxtpl import DocxTemplate
 
 TEMPLATES_DIR = Path().absolute() / 'templates'
+
+variables = {}
+
+templates = TEMPLATES_DIR.iterdir()
+for template in templates:
+    doc = DocxTemplate(template)
+    context_variables = doc.undeclared_template_variables
+    variables[template] = context_variables
+
+print(variables)
+
+
 RESULT_DIR = Path().absolute() / 'results'
 TEST_FILE = TEMPLATES_DIR / 'test.docx'
 RESULT = RESULT_DIR / 'result.docx'
 
 doc = DocxTemplate(TEST_FILE)
-# context_variables = doc.undeclared_template_variables
-
 context = {
     'фио': "Петров М. М.",
     'дни': "14",
@@ -17,3 +27,8 @@ context = {
 }
 doc.render(context)
 doc.save(RESULT)
+
+
+# TODO:
+#  1. БД
+#  2. Разбить на модули
